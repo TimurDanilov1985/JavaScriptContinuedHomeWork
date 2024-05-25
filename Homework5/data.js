591fe724dd9e58cb0438fe556e98213cc081efbf
Timur Danilov <timur.danilov.1985@mail.ru>
2024-05-25 14:25:24 +0300

    решение домашнего задания 5

---
+++ Homework5/data.js
@@ -0,0 +1,23 @@
+const dataFile = `[
+    {
+        "id": 1,
+        "url": "img/generator.jpg",
+        "name": "Генератор на бензине EMG350",
+        "description": "Energy Motors EMG350 генератор на бензине выходное напряжение 220 вольт мощность 3кВт",
+        "price": 17000
+    },
+    {
+        "id": 2,
+        "url": "img/generator1.jpg",
+        "name": "Генератор на бензине Champion",
+        "description": "Champion генератор на бензине выходное напряжение 220 в мощность 2кВт",
+        "price": 12000
+    },
+    {
+        "id": 3,
+        "url": "img/generator2.jpg",
+        "name": "Генератор на бензине Carver",
+        "description": "Генератор бензиновый Carver PPG-2500A, 2.3 кВт, 220 В, стартер ручной, бак 15 л",
+        "price": 18000
+    }
+]`;
